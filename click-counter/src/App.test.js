9c553777fe94f438a3fb44a1e332d506f2abd4f8
Chase Williams <chasewilliams@clearwateranalytics.com>
2019-07-30 15:27:53 -0600

    Added clarifying notes

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16'
-
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -12,7 +11,8 @@ test('renders without error', () => {
 });
 
 test('logged in false', () => {
-	jest.mock('./Auth.js')
+	jest.mock('./Auth.js'); //Trying to get `auth.isLoggedIn() === false`
 	const wrapper = mount(<App />);
+	console.log(wrapper.debug())
 	expect(wrapper.exists("[data-test='Logged-In-False']")).toBe(true);
 })
